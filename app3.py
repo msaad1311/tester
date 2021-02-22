b70a4a29116e0e2059ddf67fb58316819bc11263
Muhammad Saad <50722220+msaad1311@users.noreply.github.com>
2021-02-22 13:25:41 -0500

    trying out different things

--- app3.py
+++ app3.py
@@ -20,41 +20,46 @@ tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-es")
 def index():
     return render_template('home.html')
 
-# @socketio.on('speech')
-# def speech(message):
-#     message = message['audio']['dataURL'].replace('data:audio/wav;base64,','')
-#     print(type(message))
-#     # sbuf = io.StringIO()
-#     # sbuf.write(message)
-#     # print(message)
+@app.route('/audio',methods=['GET','POST'])
+def audio():
+    return render_template('audio.html')
 
-#     # decode and convert into image
-#     b = io.BytesIO(base64.b64decode(message))
-#     print('b:',type(b))
-#     r = sr.Recognizer()
-#     if os.path.isfile(fileName):
-#         os.remove(fileName)
-#     try:
-#         with open(fileName, mode='bx') as f:
-#             f.write(b.getvalue())
-#         f.close()
-#     except: 
-#         pass
+@socketio.on('speech')
+def speech(message):
+    message = message['audio']['dataURL'].replace('data:audio/wav;base64,','')
+    print(type(message))
+    # sbuf = io.StringIO()
+    # sbuf.write(message)
+    # print(message)
+
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(message))
+    print('b:',type(b))
+    r = sr.Recognizer()
+    if os.path.isfile(fileName):
+        os.remove(fileName)
+    try:
+        with open(fileName, mode='bx') as f:
+            f.write(b.getvalue())
+        f.close()
+    except: 
+        pass
     
-#     with sr.AudioFile(fileName) as source:
-#         print('into the reusable')
-#         data= r.record(source)
-#         try:
-#             text = r.recognize_google(data)
-#             print(text)
-#             translation = pipeline("translation_en_to_es", model=model, tokenizer=tokenizer)
-#             translated = translation(text)[0]['translation_text']
-#             print(translated)
-#             result = [text,translated]
-#             emit('messageBack', result)
-#         except Exception as e:
-#             emit('errorMessage')
-#             print("Exception: "+str(e))
+    with sr.AudioFile(fileName) as source:
+        print('into the reusable')
+        data= r.record(source)
+        try:
+            text = r.recognize_google(data)
+            print(text)
+            translation = pipeline("translation_en_to_es", model=model, tokenizer=tokenizer)
+            translated = translation(text)[0]['translation_text']
+            print(translated)
+            result = [text,translated]
+            emit('messageBack', result)
+        except Exception as e:
+            emit('errorMessage')
+            print("Exception: "+str(e))
+
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, host='127.0.0.1')
--- static/js/app.js
+++ static/js/app.js
@@ -1,62 +1,131 @@
-!(function () {
-  window;
-  const e = document.documentElement;
-  if (
-    (e.classList.remove("no-js"),
-    e.classList.add("js"),
-    document.body.classList.contains("has-animations"))
-  ) {
-    (window.sr = ScrollReveal()).reveal(".feature, .pricing-table-inner", {
-      duration: 600,
-      distance: "20px",
-      easing: "cubic-bezier(0.5, -0.01, 0, 1.005)",
-      origin: "bottom",
-      interval: 100,
-    }),
-      e.classList.add("anime-ready"),
-      anime
-        .timeline({ targets: ".hero-figure-box-05" })
-        .add({
-          duration: 400,
-          easing: "easeInOutExpo",
-          scaleX: [0.05, 0.05],
-          scaleY: [0, 1],
-          perspective: "500px",
-          delay: anime.random(0, 400),
-        })
-        .add({ duration: 400, easing: "easeInOutExpo", scaleX: 1 })
-        .add({
-          duration: 800,
-          rotateY: "-15deg",
-          rotateX: "8deg",
-          rotateZ: "-1deg",
-        }),
-      anime
-        .timeline({ targets: ".hero-figure-box-06, .hero-figure-box-07" })
-        .add({
-          duration: 400,
-          easing: "easeInOutExpo",
-          scaleX: [0.05, 0.05],
-          scaleY: [0, 1],
-          perspective: "500px",
-          delay: anime.random(0, 400),
-        })
-        .add({ duration: 400, easing: "easeInOutExpo", scaleX: 1 })
-        .add({ duration: 800, rotateZ: "20deg" }),
-      anime({
-        targets:
-          ".hero-figure-box-01, .hero-figure-box-02, .hero-figure-box-03, .hero-figure-box-04, .hero-figure-box-08, .hero-figure-box-09, .hero-figure-box-10",
-        duration: anime.random(600, 800),
-        delay: anime.random(600, 800),
-        rotate: [
-          anime.random(-360, 360),
-          function (e) {
-            return e.getAttribute("data-rotation");
-          },
-        ],
-        scale: [0.7, 1],
-        opacity: [0, 1],
-        easing: "easeInOutExpo",
+const socketio = io();
+let canvas = document.querySelector('canvas');
+let canvasCtx = canvas.getContext("2d");
+let analyser = null;
+const socket = socketio.on("connect", function () {
+  // reset the recorder
+  startRecording.disabled = false;
+});
+
+const startRecording = document.getElementById("start-recording");
+const stopRecording = document.getElementById("stop-recording");
+let recordAudio;
+
+// on start button handler
+startRecording.onclick = function () {
+  // recording started
+  startRecording.disabled = true;
+
+  // make use of HTML 5/WebRTC, JavaScript getUserMedia()
+  // to capture the browser microphone stream
+  navigator.getUserMedia(
+    {
+      audio: true,
+    },
+    function (stream) {
+      recordAudio = RecordRTC(stream, {
+        type: "audio",
+        mimeType: "audio/webm",
+        sampleRate: 44100, // this sampleRate should be the same in your server code
+
+        // MediaStreamRecorder, StereoAudioRecorder, WebAssemblyRecorder
+        // CanvasRecorder, GifRecorder, WhammyRecorder
+        recorderType: StereoAudioRecorder,
+
+        // Dialogflow / STT requires mono audio
+        numberOfAudioChannels: 1,
+
+        // get intervals based blobs
+        // value in milliseconds
+        // as you might not want to make detect calls every seconds
+        timeSlice: 4000,
+
+        // only for audio track
+        // audioBitsPerSecond: 128000,
+
+        // used by StereoAudioRecorder
+        // the range 22050 to 96000.
+        // let us force 16khz recording:
+        desiredSampRate: 16000,
       });
-  }
-})();
+
+      recordAudio.startRecording();
+      stopRecording.disabled = false;
+      visualize();
+    },
+    function (error) {
+      console.error(JSON.stringify(error));
+    }
+  );
+};
+
+// on stop button handler
+stopRecording.onclick = function () {
+  // recording stopped
+  startRecording.disabled = false;
+  stopRecording.disabled = true;
+
+  // stop audio recorder
+  recordAudio.stopRecording(function () {
+    // after stopping the audio, get the audio data
+    recordAudio.getDataURL(function (audioDataURL) {
+      var files = {
+        audio: {
+          type: recordAudio.getBlob().type || "audio/wav",
+          dataURL: audioDataURL,
+        },
+      };
+      socketio.emit("speech", files);
+    });
+  });
+};
+socket.on("messageBack", function (text) {
+  console.log("hi", text);
+  const transcriptID = document.getElementById("transcript");
+  const translateID = document.getElementById("translate");
+
+  transcriptID.value = text[0];
+  transcriptID.readOnly = "true";
+
+  translateID.value = text[1];
+  translateID.readOnly = "true";
+});
+socket.on("errorMessage", () => {
+  alert("Sorry retry");
+});
+
+function visualize() {
+  WIDTH = canvas.width;
+  HEIGHT = canvas.height;
+  CENTERX = canvas.width / 2;
+  CENTERY = canvas.height / 2;
+
+  recordAudio.fftSize = 32;
+  let bufferLength = recordAudio.frequencyBinCount;
+  console.log(bufferLength);
+  let dataArray = new Uint8Array(bufferLength);
+
+  canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  let draw = () => {
+    drawVisual = requestAnimationFrame(draw);
+
+    recordAudio.getByteFrequencyData(dataArray);
+    canvasCtx.fillStyle = "rgb(0, 0, 0)";
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    // let radius = dataArray.reduce((a,b) => a + b) / bufferLength;
+    let radius = dataArray[2] / 2;
+    if (radius < 20) radius = 20;
+    if (radius > 100) radius = 100;
+    // console.log('Radius ', radius)
+    canvasCtx.beginPath();
+    canvasCtx.arc(CENTERX, CENTERY, radius, 0, 2 * Math.PI, false);
+    // canvasCtx.fillStyle = 'rgb(50,50,' + (radius+100) +')';
+    // canvasCtx.fill();
+    canvasCtx.lineWidth = 6;
+    canvasCtx.strokeStyle = "rgb(50,50," + (radius + 100) + ")";
+    canvasCtx.stroke();
+  };
+  draw();
+}
